2bb23ae2f4ea2d4138ac67404544a05263f5953b
Pinaki Ray <pinaki.bpd2001@gmail.com>
2022-02-22 15:45:11 +0530

    java

--- day2/even_odd.java
+++ day2/even_odd.java
@@ -1,9 +1,16 @@
 package day2;
 public class even_odd {
     public static void main(String[] args) {
-        for(int i = 0;i<=100;i++)
+        for(int i = 1;i<=100;i++)
         {
-            System.out.println(x);
+            if(i%2==0)
+            {
+                System.out.printf("%d is even\n" , i);
+            }
+            else
+            {
+                System.out.printf("%d is odd\n" , i);
+            }
         }
     }
 }
